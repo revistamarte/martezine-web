0629018759538d6af7bf4bd9afdd16f5d1a66239
victovito <victoreosr@gmail.com>
2023-07-30 22:37:09 -0300

    footer theme correction

--- client/src/components/marteFooter/MarteFooter.js
+++ client/src/components/marteFooter/MarteFooter.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import pinterest from "../../assets/icons/pinterest-icon.png";
 import instagram from "../../assets/icons/instagram-icon.png";
 import mail from "../../assets/icons/mail-icon.png";
+import { useContext } from 'react';
+import AppContext from '../../contexts/App.context';
 
 
 import "./MarteFooter.scss"
@@ -11,10 +13,10 @@ import "./MarteFooter.scss"
 
 
 function MarteFooter() {
-
+    const {theme} = useContext(AppContext);
 
     return(
-        <div className="marte-footer">
+        <div className={`marte-footer ${theme}`}>
             
             <div className='logo-and-text'>
                 <Link to="/" className='logoM'>
