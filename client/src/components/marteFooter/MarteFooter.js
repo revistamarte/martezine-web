eb1a0d4cfb343849754756b4c0f50bb42063f1eb
tomaee <tomastrindade@gmail.com>
2023-07-31 00:06:08 -0300

    color change icons

--- client/src/components/marteFooter/MarteFooter.js
+++ client/src/components/marteFooter/MarteFooter.js
@@ -1,8 +1,11 @@
 import MarteMLogo from '../marteMLogo/MarteMLogo';
+import InstagramIcon from '../socialIcons/instagramIcon/InstagramIcon';
+import MailIcon from '../socialIcons/mailIcon/MailIcon';
+import PinterestIcon from '../socialIcons/pinterestIcon/PinterestIcon';
 import { Link } from 'react-router-dom';
-import pinterest from "../../assets/icons/pinterest-icon.png";
+/*import pinterest from "../../assets/icons/pinterest-icon.png";
 import instagram from "../../assets/icons/instagram-icon.png";
-import mail from "../../assets/icons/mail-icon.png";
+import mail from "../../assets/icons/mail-icon.png";*/
 import { useContext } from 'react';
 import AppContext from '../../contexts/App.context';
 
@@ -34,22 +37,22 @@ function MarteFooter() {
             </div>
             <div className='social-and-contracts'>
                 <div className='clicable-contract-links'>
-                    <span className='user-terms'>
+                    <span className={`user-terms ${theme}`}>
                     termos de uso
                     </span>
-                    <span>
+                    <span className={`user-terms ${theme}`}>
                     política de privacidade
                     </span>
                 </div>
                 <div className='social-logos'>
                     <a href='https://br.pinterest.com/revistamarte/' target="_blank">
-                        <button><img src={pinterest}/></button>
+                        <button><PinterestIcon/></button>
                     </a>
                     <a href='https://www.instagram.com/marte.zine/' target="_blank">
-                        <button><img src={instagram}/></button>
+                        <button><InstagramIcon/></button>
                     </a>
                     <a href='https://www.youtube.com/watch?v=z7rxl5KsPjs' target="_blank">
-                        <button><img src={mail}/></button>
+                        <button><MailIcon/></button>
                     </a>
                     
                     
